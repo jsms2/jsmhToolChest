be2f09d465737220f3762bbdadcbc92bab6828e9
jsms2 <jsgzem@163.com>
2023-04-02 16:41:32 +0800

    5.0B9

--- jsmhToolChest/Resource1.Designer.cs
+++ jsmhToolChest/Resource1.Designer.cs
@@ -130,7 +130,7 @@ namespace jsmhToolChest {
         }
         
         /// <summary>
-        ///   查找类似 5.0B8 的本地化字符串。
+        ///   查找类似 5.0B9 的本地化字符串。
         /// </summary>
         internal static string Version {
             get {
